adcdb1fbb37b91d1afb31a9c1003b664875bd4f6
José Malcher Jr <contato@josemalcher.net>
2020-11-19 15:24:46 -0300

    03 - Laravel API - Login no Web Service

--- php-client/public/index.php
+++ php-client/public/index.php
@@ -2,4 +2,29 @@
 
 include_once '../bootstrap/start.php';
 
-echo 'teste';
+login();
+
+
+function login(){
+
+    $email = 'jose@josemalcher.net';
+    $pass = '123456';
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => [
+            'email'=> $email,
+            'password'=> $pass
+        ],
+        CURLOPT_URL => 'http://127.0.0.1:8000/api/V1/auth'
+    ]);
+
+    $response = json_decode(curl_exec($curl));
+    curl_close($curl);
+
+    return $response->token;
+
+}
